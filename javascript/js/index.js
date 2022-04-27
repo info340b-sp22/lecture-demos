440673fadb8d99151320b7a9c0efe4b25691ec5a
Tim Carlson <timca@dashplatform.com>
2022-04-26 19:07:46 -0700

    end of lecture:

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,77 +1,103 @@
+console.log("hello world");
+console.log("hello back to you");
+'use strict';
 
-'use strict'
+// let array = [1,2,3]
+// console.log(array)
 
-console.log("hello world!"); //prints 
+// function sayHello(){
+//     console.log("Hello");
+// }
 
+// console.log(sayHello)
+// <!-- assign array to variable -->
+let myArray = ['a','b','c'];
 
-// let x = 'hello';
-// console.log(typeof x)
-// x=42;
-// console.log(typeof x);
+typeof myArray //=> 'object'
+console.log(typeof myArray);
+let other = myArray;
 
-// let hourSlept;
-// console.log(hourSlept);
-const myArray = [1, 2, 3, 4]
-for (let i=0; i < myArray.length; i++)  
-{
-    console.log(myArray[i])
+// <!-- access value in other -->
+console.log( other[1] ); 
+
+//takes in TWO callback functions!
+function doTogether(firstCallback, secondCallback){
+    firstCallback();  //execute the first function
+    secondCallback();  //execute the second function
+    console.log('at the same time!');
 }
 
-for(let theItem of myArray) { //or loop through array items 
-    console.log(theItem)
-  }
-// const x = 4;
-// x = 5; // TypeError: Assignment to constant variable.
-
-// let y = 1.5;
-// y = 2; // ok!
-
-
-// const dinnerOptions = [
-//     ['chicken', 'mashed potatoes', 'mixed veggies'],
-//     ['steak', 'seasoned potatoes', 'asparagus'],
-//     ['fish', "rice", 'green beans'],
-//     ['portobello steak', 'rice', 'green beans']
-// ];
-
-// const agesObj = {sara:42, amit:35, zhang:13};
-// console.log("object:", agesObj);
-// console.log("keys", Object.keys(agesObj));
-// console.log("object:" + agesObj);
-
-// const person = {
-//     firstName: 'Alice',
-//     lastName: 'Smith',
-//     age: 40,
-//     favorites: {
-//       music: 'jazz',
-//       food: 'pizza',
-//       numbers: [12, 42]
-//     }
-//   };
-
-// const myArray = [1,2,3];
-// myArray[0] = 5;
-// console.log(myArray);
-  
-//   console.log(person['firstName']+' '+person['lastName']); //"Alice Jones"
-//   const inputtedValue = "food"
- 
-  
-//   const favFood = person['favorites'][inputtedValue]; //object in the object
-                
-//   console.log(favFood);
- 
-// const peopleTable = [
-//     {name: 'Ada', height: 64, weight: 135},
-//     {name: 'Bob', height: 74, weight: 156},
-//     {name: 'Chris', height: 69, weight: 139},
-//     {name: 'Diya', height: 69, weight: 144},
-//     {name: 'Emma', height: 71, weight: 152}
-// ]
-
-// console.log(peopleTable[3].height);
-
-// for (let i=1; i<peopleTable.length; i++){
-//     console.log(peopleTable[i].weight);
+// function patHead() {
+//     console.log('pat your head');
 // }
+
+function rubBelly() {
+    console.log('rub your belly');
+}
+
+//pass in the callbacks to do them together
+doTogether(rubBelly, patHead);
+
+// let patHead = function() {
+//     console.log('pat your head');
+// }
+
+function sayHello(name) { 
+    console.log("Hello, "+name);
+ }
+ 
+ //takes ANOTHER FUNCTION as an arg
+ //will call the arg function, 
+ //passing it "world"
+ function doWorld(aFunction) {
+    aFunction("world");
+ }
+ 
+ 
+ 
+ //call function and pass value
+ doWorld(sayHello); //prints "Hello world"
+ 
+ 
+ //pass literal (anonymous value)
+ doWorld(function(msg) {
+   console.log("you said: "+msg);
+ }); //prints "you said: world"
+ 
+
+function patHead() {
+    console.log('pat your head');
+}
+
+let myArray1 = [1,2,3,4,5];
+
+for(item of myArray1){
+    console.log(item);
+}
+
+let sum = 0;
+myArray1.forEach(function(n){
+    sum = sum + n;
+});
+console.log(sum);
+
+
+myArray1.forEach(function(n, idx){
+    myArray1[idx] = n*n;
+});
+console.log(myArray1);
+
+myArray1 = myArray1.map(function(n) {
+    return n*n;
+})
+console.log(myArray1);
+
+let add = function(total, next){
+    return total+next;
+}
+
+let myReduction = myArray1.reduce(function(total, next){
+    return total+next;
+}, 5)
+console.log(myReduction);
+
