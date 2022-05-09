ab16b74131e92d2635360aa0a1e345fd5d7e90c4
Tim Carlson <timca@dashplatform.com>
2022-05-09 15:48:40 -0700

    react-components lecture start

---
+++ react-demo/src/components/App.js
@@ -0,0 +1,21 @@
+
+//import from other components; Messages.js, etc
+import { MessageList } from './Messages'
+
+//declare a function component
+export default function App() {
+  return (
+    <MessageList /> 
+  )
+}
+
+// //import from other components; Messages.js, etc
+// import { MessageList } from './Messages'
+// const messagesArray = ["Hello world", "No borders", "Go huskies!"];
+
+
+// export default function App() {
+//   return (
+//     <MessageList messages={messagesArray} /> 
+//   )
+// }
