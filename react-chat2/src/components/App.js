892c66a0531e1b1fe95fd55bbadd759d7d9b9b3c
Tim Carlson <timca@dashplatform.com>
2022-05-13 10:27:00 -0700

    cleaned up chat2 also fixed the like icon

--- react-chat2/src/components/App.js
+++ react-chat2/src/components/App.js
@@ -1,81 +1,4 @@
-// import React from 'react';
 
-// import HeaderBar from './HeaderBar';
-// import ChannelList from './ChannelList';
-// import MessagePane from './MessagePane';
-// import ComposeForm from './ComposeForm';
-
-// function App(props) {
-
-//   //data!
-//   const channelList = ['general', 'random', 'social', 'birbs', 'channel-5']
-  
-//   const currentChannel ='general'];
-
-//   // //example 1
-//   // const currentChannel ='unknown';
-
-//   return (
-//     <div className="container-fluid d-flex flex-column">
-//       <HeaderBar />
-//       <div className="row flex-grow-1">
-//         <div className="col-3">
-//           <ChannelList channels={channelList} />
-//         </div>
-//         <main className="col d-flex flex-column chat-column">
-//           <div className="chat-pane">
-//             <MessagePane channel={currentChannel} />
-//           </div>
-//         </main>
-//         <ComposeForm />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// //Example 1
-// import React from 'react';
-
-// import HeaderBar from './HeaderBar';
-// import ChannelList from './ChannelList';
-// import MessagePane from './MessagePane';
-// import ComposeForm from './ComposeForm';
-
-// function App(props) {
-
-//      //data!
-//   const channelList = ['general', 'random', 'social', 'birbs', 'channel-5']
-  
-//   const currentChannel ='general';
-
-//   // //example 1
-//   // const currentChannel ='unknown';
-
-//   const showing = true;
-
-//   return (
-//     <div className="container-fluid d-flex flex-column">
-//       <HeaderBar />
-//       <div className="row flex-grow-1">
-//         <div className="col-3">
-//           <ChannelList channels={channelList} />
-//         </div>
-//         <main className="col d-flex flex-column chat-column">
-//           <div className="chat-pane">
-//             <MessagePane channel={currentChannel} />
-//           </div>
-//         </main>
-//         <ComposeForm />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//Example 6
 import React, {useState} from 'react';
 
 import HeaderBar from './HeaderBar';
@@ -87,10 +10,10 @@ import CHAT_LOG from '../data/chat_log.json';                       // Example 6
 
 function App(props) {
 
-  const [messagesArray, setMessagesArray] = useState(CHAT_LOG);       // EXample 6: moved from state up from messages
+  const [messagesArray, setMessagesArray] = useState(CHAT_LOG);       // Example 6: moved from state up from messages
 
 //add new message
-const addMessage = (userId, userName, messageText, channel) => {                      //example 6 moves to app to elevate state
+const addMessage = (userId, userName, messageText, channel) => {       //Example 6 moves to app to elevate state
 
   const newMessage = {
     userId: userId,
@@ -111,9 +34,6 @@ const addMessage = (userId, userName, messageText, channel) => {                
   
   const currentChannel ='general';
 
-  // //example 1
-  // const currentChannel ='unknown';
-
   const showing = true;
 
   return (
@@ -128,7 +48,7 @@ const addMessage = (userId, userName, messageText, channel) => {                
             <MessagePane messagesArray={messagesArray} channel={currentChannel} />      {/*Example 6 */}
           </div>
         </main>
-        <ComposeForm whatToDoWhenSubmitted={addMessage} />      {/*Example 6 */}
+        <ComposeForm whatToDoWhenSubmitted={addMessage} />                            {/*Example 6 */}
       </div>
     </div>
   );
