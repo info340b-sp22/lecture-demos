cdea97ab12bbe7dbb861b9903293995514718eb0
Tim Carlson <timca@dashplatform.com>
2022-05-25 09:48:12 -0700

    post lecture

--- react-ajax/src/App.js
+++ react-ajax/src/App.js
@@ -1,4 +1,335 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+
+// const EXAMPLE_DATA = [
+//   { name: "react", html_url: "https://github.com/factbook/react" },
+//   { name: "react-bootstrap", html_url: "https://github.com/react-boostrap/react-bootstrap" },
+//   { name: "react-router", html_url: "https://github.com/remix-run/react-router" }
+// ];
+
+
+// function App() {
+//   const [data, setData] = useState(EXAMPLE_DATA);
+
+//   //control form
+//   const [queryInput, setQueryInput] = useState('');
+
+//   const handleChange = (event) => {
+//     setQueryInput(event.target.value);
+//   }
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//   }
+
+//   const dataElemArray = data.map((repo) => {
+//     return <li key={repo.html_url}> <a href={repo.html_url}>{repo.name}</a> </li>
+//   });
+
+
+//   return (
+
+//     <div className="container">
+//       <header> <h1> AJAX Demo</h1></header>
+
+//       <form method="Post" action="https://api.github.com/search/repositories">
+//         <input type="text" className="form-control mb-2"
+//           name="q"
+//           placeholder="Search Github for..."
+//           value={queryInput} onChange={handleChange}
+//         />
+//         {/* Example 1*/}
+//         <input type="hidden" name="sort" value="stars"/>
+//         {/* <input type="hidden" name="sort" value="best_match"/> */}
+
+//         <button type="submit" className="btn btn-primary">Seach!</button>
+//       </form>
+
+//       <div className="mt-4">
+//         <h2>Results</h2>
+//         {/* results go here */}
+//         {dataElemArray}
+
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// //Example 10
+// import React, { useState } from 'react';
+
+// const EXAMPLE_DATA = [
+//   { name: "react", html_url: "https://github.com/factbook/react" },
+//   { name: "react-bootstrap", html_url: "https://github.com/react-boostrap/react-bootstrap" },
+//   { name: "react-router", html_url: "https://github.com/remix-run/react-router" }
+// ];
+
+
+// function App() {
+//   //Example 10 - set the initial state variable to be empty array
+//   const [data, setData] = useState([]);
+
+  
+//   const [queryInput, setQueryInput] = useState('');
+
+//   const handleChange = (event) => {
+//     setQueryInput(event.target.value);
+//   }
+
+//   const handleSubmit = (event) => {
+
+//     // //Example 10- 
+//     event.preventDefault();
+//     //Example 10 - add the queryInput state variable to the fetch param
+//     //note that the setData is using 'data.items', not just 'data'
+//     //note that i'm using the queryInput state value too!
+//     fetch("https://api.github.com/search/repositories?q=" + queryInput)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log("received: ", data);
+//         setData(data.items);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       })
+//   }
+
+//   //rendering the data
+
+//   //Example 10
+//   console.log("rendering withdata", data);
+//   const dataElemArray = data.map((repo) => {
+//     return <li key={repo.html_url}> <a href={repo.html_url}>{repo.full_name}</a> </li>
+//   });
+
+
+//   return (
+
+//     <div className="container">
+//       <header> <h1> AJAX Demo</h1></header>
+//   {/*Example 10 add the onSubmit to the form  */}
+//       <form method="GET" action="https://api.github.com/search/repositories" onSubmit={handleSubmit}>
+//         <input type="text" className="form-control mb-2"
+//           name="q"
+//           placeholder="Search Github for..."
+//           value={queryInput} onChange={handleChange}
+//         />
+//         <input type="hidden" name="sort" value="best_match" />
+
+//         <button type="submit" className="btn btn-primary">Seach!</button>
+//       </form>
+
+// {/* Example 10, conditional rendering if there's no data yet*/}
+//       {data.length === 0 && <p>No results yet</p>}
+//       {data.length !== 0 &&
+//         <div className="mt-4">
+//           <h2>Results</h2>
+
+//           {dataElemArray}
+
+//         </div>
+//       }
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// // Example 11
+
+// //Example 11 - import the useEffect hook component
+// import React, { useState, useEffect } from 'react';
+
+// const EXAMPLE_DATA = [
+//   { name: "react", html_url: "https://github.com/factbook/react" },
+//   { name: "react-bootstrap", html_url: "https://github.com/react-boostrap/react-bootstrap" },
+//   { name: "react-router", html_url: "https://github.com/remix-run/react-router" }
+// ];
+
+
+// function App() {
+//   const [data, setData] = useState([]);
+//   const [queryInput, setQueryInput] = useState('');
+
+// //Example 11 - put the fetch inside of the useEffect
+// useEffect(function() {
+//   //what to do when the app first loads
+//     fetch("https://api.github.com/search/repositories?q=react")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log("received: ", data);
+//       setData(data.items);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+
+// }, []);
+
+//   const handleChange = (event) => {
+//     setQueryInput(event.target.value);
+//   }
+
+//   const handleSubmit = (event) => {
+
+    
+//     event.preventDefault();
+    
+//     fetch("https://api.github.com/search/repositories?q=react" + queryInput)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log("received: ", data);
+//         setData(data.items);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       })
+//   }
+
+//   //rendering the data
+
+//     console.log("rendering withdata", data);
+//   const dataElemArray = data.map((repo) => {
+//     return <li key={repo.html_url}> <a href={repo.html_url}>{repo.full_name}</a> </li>
+//   });
+
+//   return (
+//     <div className="container">
+//       <header> <h1> AJAX Demo</h1></header>
+//       <form method="GET" action="https://api.github.com/search/repositories" onSubmit={handleSubmit}>
+//         <input type="text" className="form-control mb-2"
+//           name="q"
+//           placeholder="Search Github for..."
+//           value={queryInput} onChange={handleChange}
+//         />
+//         <input type="hidden" name="sort" value="best_match" />
+
+//         <button type="submit" className="btn btn-primary">Seach!</button>
+//       </form>
+
+//       {data.length === 0 && <p>No results yet</p>}
+//       {data.length !== 0 &&
+//         <div className="mt-4">
+//           <h2>Results</h2>
+//           {/* results go here */}
+//           {dataElemArray}
+
+//         </div>
+//       }
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// //Example 12
+
+// //example 12 - second argument array
+// import React, { useState, useEffect } from 'react';
+
+// const EXAMPLE_DATA = [
+//   { name: "react", html_url: "https://github.com/factbook/react" },
+//   { name: "react-bootstrap", html_url: "https://github.com/react-boostrap/react-bootstrap" },
+//   { name: "react-router", html_url: "https://github.com/remix-run/react-router" }
+// ];
+
+
+// function App(props) {
+//   const [data, setData] = useState([]);
+//   const [queryInput, setQueryInput] = useState('');
+//   console.log("props : ", props.initialSearch);
+
+// // Example 12 modify to arrow function for coding style
+// useEffect(() => {
+  
+//     fetch("https://api.github.com/search/repositories?q="+props.initialSearch)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log("received: ", data);
+//       setData(data.items);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+// //Example 12 - if somehow this prop changes
+// }, [props.initialSearch]);
+
+//   const handleChange = (event) => {
+//     setQueryInput(event.target.value);
+//   }
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+    
+//     fetch("https://api.github.com/search/repositories?q=react" + queryInput)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log("received: ", data);
+//         setData(data.items);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       })
+//   }
+
+//   //rendering the data
+
+//     console.log("rendering withdata", data);
+//   const dataElemArray = data.map((repo) => {
+//     return <li key={repo.html_url}> <a href={repo.html_url}>{repo.full_name}</a> </li>
+//   });
+
+
+//   return (
+
+//     <div className="container">
+//       <header> <h1> AJAX Demo</h1></header>
+//       <form method="GET" action="https://api.github.com/search/repositories" onSubmit={handleSubmit}>
+//         <input type="text" className="form-control mb-2"
+//           name="q"
+//           placeholder="Search Github for..."
+//           value={queryInput} onChange={handleChange}
+//         />
+//         <input type="hidden" name="sort" value="best_match" />
+
+//         <button type="submit" className="btn btn-primary">Seach!</button>
+//       </form>
+
+//       {data.length === 0 && <p>No results yet</p>}
+//       {data.length !== 0 &&
+//         <div className="mt-4">
+//           <h2>Results</h2>
+//           {/* results go here */}
+//           {dataElemArray}
+
+//         </div>
+//       }
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+//Example 13
+
+//example 13 - Cleanup
+import React, { useState, useEffect } from 'react';
 
 const EXAMPLE_DATA = [
   { name: "react", html_url: "https://github.com/factbook/react" },
@@ -7,22 +338,61 @@ const EXAMPLE_DATA = [
 ];
 
 
-function App() {
-  const [data, setData] = useState(EXAMPLE_DATA);
-
-  //control form
+function App(props) {
+  const [data, setData] = useState([]);
   const [queryInput, setQueryInput] = useState('');
+
+
+useEffect(() => {
+  //what to do when the app first loads
+    // fetch("https://api.github.com/search/repositories?q="+props.initialSearch)
+    fetch("/myfile.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("received: ", data);
+      setData(data.items);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+
+    function cleanup() {
+      console.log("component is being removed")
+      console.log("turn off the lights")
+    }
+    return cleanup;
+
+}, [props.initialSearch]);
 
   const handleChange = (event) => {
     setQueryInput(event.target.value);
   }
 
   const handleSubmit = (event) => {
+
+    
     event.preventDefault();
+    
+    fetch("https://api.github.com/search/repositories?q=react" + queryInput)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("received: ", data);
+        setData(data.items);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
+  //rendering the data
+
+    console.log("rendering withdata", data);
   const dataElemArray = data.map((repo) => {
-    return <li key={repo.html_url}> <a href={repo.html_url}>{repo.name}</a> </li>
+    return <li key={repo.html_url}> <a href={repo.html_url}>{repo.full_name}</a> </li>
   });
 
 
@@ -30,28 +400,29 @@ function App() {
 
     <div className="container">
       <header> <h1> AJAX Demo</h1></header>
-
-      <form method="GET" action="https://api.github.com/search/repositories">
+      <form method="GET" action="https://api.github.com/search/repositories" onSubmit={handleSubmit}>
         <input type="text" className="form-control mb-2"
           name="q"
           placeholder="Search Github for..."
           value={queryInput} onChange={handleChange}
         />
-        {/* Example 1*/}
-        {/* <input type="hidden" name="sort" value="stars"/> */}
-        <input type="hidden" name="sort" value="best_match"/>
+        <input type="hidden" name="sort" value="best_match" />
 
         <button type="submit" className="btn btn-primary">Seach!</button>
       </form>
 
-      <div className="mt-4">
-        <h2>Results</h2>
-        {/* results go here */}
-        {dataElemArray}
+      {data.length === 0 && <p>No results yet</p>}
+      {data.length !== 0 &&
+        <div className="mt-4">
+          <h2>Results</h2>
+          {/* results go here */}
+          {dataElemArray}
 
-      </div>
+        </div>
+      }
     </div>
   );
 }
 
 export default App;
+
--- react-ajax/src/index.js
+++ react-ajax/src/index.js
@@ -1,11 +1,168 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
+// // Example 1
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
+
+// //Example 1
+// console.log("About to send");
+
+// const result = fetch("https://api.github.com/search/repositories?q=react");
+// console.log("Request sent");
+// console.log(result);
+
+//Example 2 - We get back a promise, not a result
+
+// //Example 2 Callback for the .then()
+// function whatToDoWhenFulfilled() {
+//     console.log("hey it's my turn");
+// }
+
+// console.log("About to send");
+// const fetchPromise = fetch("https://api.github.com/search/repositories?q=react");
+// //Example 2 - we can call a method off of a promise called .then and provide a callback
+// fetchPromise.then(whatToDoWhenFulfilled);
+
+
+
+
+// //Example 3 - look at what's in the response
+//     // Example 3, log out what the response is, and note the timing
+// function whatToDoWhenFulfilled(response) {
+//     console.log("hey it's my turn");
+//     console.log(response);
+// }
+// console.log("About to send");
+// const fetchPromise = fetch("https://api.github.com/search/repositories?q=react");
+// fetchPromise.then(whatToDoWhenFulfilled);
+
+// console.log("Request sent");
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// // root.render(
+// //     <App />
+// // );
+
+
+// //Example 4 - pass as an anonymous function
+
+// console.log("About to send");
+// const fetchPromise = fetch("https://api.github.com/search/repositories?q=react");
+
+// //Example 4 (use an anonymous function)
+// fetchPromise.then( (response) => {
+//     console.log("promise fulfilled");
+//     console.log(response);
+// });
+
+
+// //Example 5 - extract the body from the promise
+
+// console.log("About to send");
+// const fetchPromise = fetch("https://api.github.com/search/repositories?q=react");
+// fetchPromise.then( (response) => {
+//     console.log("promise fulfilled");
+//     console.log(response);
+    
+//     //example 5 - Lets now use the .json() method to pull out the data (open the envelop)
+//     const dataPromise = response.json();
+//     console.log(dataPromise); //it's another promise
+//     console.log("started opening the letter");
+// });
+
+
+// //Example 6 - append the two promises .then()'s together
+// // note that the second promise actually has the data!
+
+// console.log("About to send");
+// const fetchPromise = fetch("https://api.github.com/search/repositories?q=react");
+// const thatDataPromise = fetchPromise.then( (response) => {
+//     console.log("promise fulfilled");
+//     console.log(response);
+//     const dataPromise = response.json();
+//     console.log(dataPromise); //it's another promise
+//     console.log("started opening the letter");
+//     return dataPromise;
+// });
+
+// thatDataPromise.then((data=> {
+//     console.log(data);
+
+// }))
+// console.log("Request sent");
+
+// //Example 7 - Clean it up
+
+// fetch("https://api.github.com/search/repositories?q=react")
+// .then((response) => {
+//     return  response.json();
+// })
+// .then((data=> {
+//     console.log(data);
+// }))
+
+// console.log("Request sent");
+
+// //Example 8 - catching errors
+
+// fetch("bad")
+// .then( (response) => {
+//     return  response.json();
+// })
+// .then((data=> {
+//     console.log(data);
+// }))
+// .catch((error)=> {
+//     console.log(error);
+// })
+// .then(() => console.log("finally"));
+// console.log("Request sent");
+
+// //Example 9 - async/await syntax
+
+// const fetchData = async () => {
+//     const response = await fetch("https://api.github.com/search/repositories?q=react");
+//     const data = await response.json();
+//     console.log(data);
+// }
+
+// fetchData();
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
+
+
+// Example 10
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <App initialSearch="d3"/>
 );
 
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
