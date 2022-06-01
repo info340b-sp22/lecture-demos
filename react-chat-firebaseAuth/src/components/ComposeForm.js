5efecb94fec1f21266c60e710ae9d3bb4d125716
Tim Carlson <timca@dashplatform.com>
2022-06-01 09:51:01 -0700

    post lecture

--- react-chat-firebaseAuth/src/components/ComposeForm.js
+++ react-chat-firebaseAuth/src/components/ComposeForm.js
@@ -1,10 +1,59 @@
+// import React, { useState } from 'react';
+
+// export default function ComposeForm(props) {
+//   const [textValue, setTextValue] = useState("");
+
+//   const currentUser = props.currentUser;
+
+
+//   const handleChange = (event) => {
+//     const typedValue = event.target.value;
+//     setTextValue(typedValue)
+//   }
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log("submitting form with", textValue);
+
+//     //arguments: userId, userName, text, channel
+//     props.whatToDoWhenSubmitted(currentUser.userId, currentUser.userName, textValue, "general");
+
+//     setTextValue('');
+//   }
+
+//   if(!currentUser.userId){
+//     return null; //show nothing for component
+//   }
+
+//   return (
+//     <form className="my-2" onSubmit={handleSubmit}>
+//       <div className="input-group">
+//         {currentUser.userName &&
+//           <img src={'/img/' + currentUser.userName + '.jpg'} alt={currentUser.userName + " avatar"} />
+//         }
+//         <textarea className="form-control" rows="2" placeholder="Type a new message" 
+//                   value={textValue}
+//                   onChange={handleChange}
+//                   disabled={!currentUser.userName}
+//                   />
+//         <button className="btn btn-secondary" type="submit" disabled={!currentUser.userName}>
+//           <span className="material-icons">send</span>
+//         </button>
+//       </div>
+//     </form>
+//   );
+// }
+
+//Example 3 - fix the avatar stuff
 import React, { useState } from 'react';
 
 export default function ComposeForm(props) {
   const [textValue, setTextValue] = useState("");
 
   const currentUser = props.currentUser;
-
+  
+  //Example 3
+  let avatarUrl = props.currentUser.photoURL || '/img/null.png'
 
   const handleChange = (event) => {
     const typedValue = event.target.value;
@@ -15,8 +64,9 @@ export default function ComposeForm(props) {
     event.preventDefault();
     console.log("submitting form with", textValue);
 
+    //Example 3 - add the photoURL
     //arguments: userId, userName, text, channel
-    props.whatToDoWhenSubmitted(currentUser.userId, currentUser.userName, textValue, "general");
+    props.whatToDoWhenSubmitted(currentUser.userId, currentUser.userName, avatarUrl, textValue, "general");
 
     setTextValue('');
   }
@@ -28,8 +78,9 @@ export default function ComposeForm(props) {
   return (
     <form className="my-2" onSubmit={handleSubmit}>
       <div className="input-group">
+       {/* Example 3*/}
         {currentUser.userName &&
-          <img src={'/img/' + currentUser.userName + '.jpg'} alt={currentUser.userName + " avatar"} />
+          <img src={avatarUrl} alt={currentUser.userName + " avatar"} />
         }
         <textarea className="form-control" rows="2" placeholder="Type a new message" 
                   value={textValue}
